61663c618a9e89eb405dbc6781b19c15416f7f80
Taha Rostami <taha.rostami.darunkola@gmail.com>
2025-12-03 10:45:12 +0100

    Update inheritance.py

--- code_mutate/ops/inheritance.py
+++ code_mutate/ops/inheritance.py
@@ -51,8 +51,15 @@ class BaseOverridenElementMutation(MutationOperator):
             if parent.type == "class_definition": break
             parent = parent.parent
 
+        if parent is None or parent.type != "class_definition":
+            return False
+
         superclasses = []
         superclasses_node = parent.child_by_field_name("superclasses")
+
+        if not superclasses_node:
+            return False
+        
         if superclasses_node:
             if superclasses_node.type == "identifier":
                 superclasses.append(superclasses_node.text.decode())
